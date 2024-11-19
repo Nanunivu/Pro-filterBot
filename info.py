36f85b5a2f354fe352d51a0aa97be4d01331b3ca
Nanunivu <nivunanu321@gmail.com>
2024-11-19 18:03:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/HA_Bots_Support')
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/shettyDiscus_Group')
 OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/jeevan_shetty")
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/shettymoviess')
 FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/shetty_moviez1')
